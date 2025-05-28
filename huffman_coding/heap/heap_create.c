6007a02ea3741881b650ac74ae1c5932b57a75e4
Asaiba <A.saiba@alustudent.com>
2025-05-28 16:53:16 +0200

    correction

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,20 +1,19 @@
 #include "heap.h"
 
 /**
- * heap_create - program that creates a new heap data structure
- * @data_cmp: a pointer to the comparison function for node data
- * Return: a pointer to the created heap_t structure, or NULL if it fails
+ * heap_create - Creates a new heap data structure
+ * @data_cmp: A pointer to the comparison function for node data
+ *
+ * Return: A pointer to the created heap_t structure, or NULL on failure
  */
-
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
-	heap_t *heap = NULL;
+	heap_t *heap;
 
 	if (data_cmp == NULL)
 		return (NULL);
 
 	heap = malloc(sizeof(heap_t));
-
 	if (heap == NULL)
 		return (NULL);
 
