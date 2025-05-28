8cd8eb1bd90d851848885de929642a8d51eb10d7
Asaiba <A.saiba@alustudent.com>
2025-05-28 17:13:17 +0200

    fourth

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,4 +1,33 @@
-#include "heap.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct heap_node_s - Node of a heap
+ * @data: Pointer to the data stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child
+ * @right: Pointer to the right child
+ */
+typedef struct heap_node_s
+{
+	void *data;
+	struct heap_node_s *parent;
+	struct heap_node_s *left;
+	struct heap_node_s *right;
+} heap_node_t;
+
+/**
+ * struct heap_s - Structure representing a heap
+ * @size: Number of nodes in the heap
+ * @root: Pointer to the root node
+ * @data_cmp: Pointer to comparison function (min/max heap logic)
+ */
+typedef struct heap_s
+{
+	size_t size;
+	heap_node_t *root;
+	int (*data_cmp)(void *, void *);
+} heap_t;
 
 /**
  * heap_create - Creates a new heap data structure
@@ -23,4 +52,20 @@ heap_t *heap_create(int (*data_cmp)(void *, void *))
 	heap->root = NULL;
 
 	return (heap);
+}
+
+/**
+ * restore_heapify - Restores the heap property from a given node downward
+ * @heap: Pointer to the heap structure
+ * @node: Pointer to the node to start heapifying from
+ *
+ * Description: This is a placeholder for restoring the heap property
+ *              after insertion or deletion. Actual logic should be implemented.
+ */
+void restore_heapify(heap_t *heap, heap_node_t *node)
+{
+	(void)heap;
+	(void)node;
+
+	/* TODO: Implement heapify logic here */
 }
