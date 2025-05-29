36b7328aa45a4492a5c5f50442918ebb9c947eb2
Cyber_001 <144014192+Asaiba@users.noreply.github.com>
2025-05-30 00:02:46 +0200

    Fifth

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,71 +1,22 @@
-#include <stdlib.h>
-#include <stdio.h>
+
+#include "heap.h"
 
 /**
- * struct heap_node_s - Node of a heap
- * @data: Pointer to the data stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child
- * @right: Pointer to the right child
+ * heap_create- A function that creates a Heap data structure
+ * @data_cmp: A pointer to a comparison function
+ * Return:  A pointer to the created heap_t structure, or NULL if it fails
  */
-typedef struct heap_node_s
+heap_t *heap_create(int (*data_cmp) (void *, void *))
 {
-	void *data;
-	struct heap_node_s *parent;
-	struct heap_node_s *left;
-	struct heap_node_s *right;
-} heap_node_t;
-
-/**
- * struct heap_s - Structure representing a heap
- * @size: Number of nodes in the heap
- * @root: Pointer to the root node
- * @data_cmp: Pointer to comparison function (min/max heap logic)
- */
-typedef struct heap_s
-{
-	size_t size;
-	heap_node_t *root;
-	int (*data_cmp)(void *, void *);
-} heap_t;
-
-/**
- * heap_create - Creates a new heap data structure
- * @data_cmp: Pointer to the comparison function for node data
- *
- * Return: A pointer to the newly created heap_t structure,
- *         or NULL if memory allocation fails or if data_cmp is NULL
- */
-heap_t *heap_create(int (*data_cmp)(void *, void *))
-{
-	heap_t *heap;
-
-	if (data_cmp == NULL)
-		return (NULL);
+	heap_t *heap = NULL;
 
 	heap = malloc(sizeof(heap_t));
+
 	if (heap == NULL)
 		return (NULL);
 
+	heap->root = NULL;
 	heap->size = 0;
 	heap->data_cmp = data_cmp;
-	heap->root = NULL;
-
 	return (heap);
-}
-
-/**
- * restore_heapify - Restores the heap property from a given node downward
- * @heap: Pointer to the heap structure
- * @node: Pointer to the node to start heapifying from
- *
- * Description: This is a placeholder for restoring the heap property
- *              after insertion or deletion. Actual logic should be implemented.
- */
-void restore_heapify(heap_t *heap, heap_node_t *node)
-{
-	(void)heap;
-	(void)node;
-
-	/* TODO: Implement heapify logic here */
 }
